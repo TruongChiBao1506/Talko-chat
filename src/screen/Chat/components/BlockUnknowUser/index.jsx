67b6fc117ab5f03877d63292d5b9b24967521a8a
Van Chi Hieu <vanchihieu3@gmail.com>
2025-02-21 21:59:49 +0700

    component blockunknown user

---
+++ src/screen/Chat/components/BlockUnknowUser/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+BlockUnKnowUser.propTypes = {};
+
+function BlockUnKnowUser(props) {
+  return <div>unknow</div>;
+}
+
+export default BlockUnKnowUser;
