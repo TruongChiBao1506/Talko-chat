cdd65ae03d6b6cce7775814e935910a5f3f2d937
Van Chi Hieu <vanchihieu3@gmail.com>
2025-02-28 19:41:28 +0700

    Component Channel

--- src/components/avatarCustom/index.jsx
+++ src/components/avatarCustom/index.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Avatar, Tooltip } from 'antd';
-import getSummaryName from '../../utils/nameHelper';
-
-function AvatarCustom({ 
-    src = "", 
-    name = "", 
-    style = {}, 
-    color = "#408ec6", 
-    size, 
-    ...rest 
-}) {
-    const avatarStyle = { backgroundColor: color, ...style };
-    const displayName = name || "Unknown";
-
-    return src ? (
-        <Avatar src={src} size={size} {...rest} />
-    ) : (
-        <Tooltip title={displayName} placement="top">
-            <Avatar style={avatarStyle} size={size} {...rest}>
-                {getSummaryName(displayName)}
-            </Avatar>
-        </Tooltip>
-    );
-}
+import React from "react";
+import PropTypes from "prop-types";
+import { Avatar, Tooltip } from "antd";
+import getSummaryName from "utils/nameHelper";
 
 AvatarCustom.propTypes = {
-    src: PropTypes.string,
-    name: PropTypes.string,
-    style: PropTypes.object,
-    size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    color: PropTypes.string,
+  src: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  style: PropTypes.object,
+  demention: PropTypes.number,
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  color: PropTypes.string,
 };
+
+AvatarCustom.defaultProps = {
+  src: "",
+  name: "",
+  style: {},
+  color: "#408ec6",
+};
+
+function AvatarCustom(props) {
+  const { src, name, style, color, ...rest } = props;
+
+  return (
+    <>
+      {src ? (
+        <Avatar {...props} />
+      ) : (
+        <Tooltip title={name} placement="top">
+          <Avatar style={{ backgroundColor: color, ...style }} {...rest}>
+            {getSummaryName(name)}
+          </Avatar>
+        </Tooltip>
+      )}
+    </>
+  );
+}
 
 export default AvatarCustom;
