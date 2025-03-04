e48710b6318c15b975dd32bca23856226703fcf4
Van Chi Hieu <vanchihieu3@gmail.com>
2025-03-04 22:42:49 +0700

    Component Message Type

---
+++ src/screen/Chat/components/MentionOption/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.scss";
+import { Mentions } from "antd";
+import PersonalIcon from "../PersonalIcon";
+
+MentionOption.propTypes = {
+  value: PropTypes.string.isRequired,
+  user: PropTypes.object,
+};
+
+MentionOption.defaultProps = {
+  user: {},
+};
+
+function MentionOption({ value, user }) {
+  const { Option } = Mentions;
+  return (
+    <Option value={value}>
+      <div className="mention-option">
+        <div className="icon-user-item">
+          <PersonalIcon demention={24} avatar={user.avatar} name={user.name} />
+        </div>
+
+        <div className="name-user-item">{user.name}</div>
+      </div>
+    </Option>
+  );
+}
+
+export default MentionOption;
