3822dc6a34abde296ce762f2c14b1387e75a80c5
TruongChiBao <chibaotruong1506@gmail.com>
2025-03-24 16:18:42 +0700

    add SuggestList

---
+++ src/screen/Friend/components/SuggestList/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import SuggestCard from '../SuggestCard'
+import { Col, Row } from 'antd';
+import UserCard from '../../../Chat/components/UserCard';
+import './style.css';
+
+SuggestList.propTypes = {
+    data: PropTypes.array,
+};
+
+function SuggestList({ data = []}) {
+    const [visible, setVisible] = useState(false);
+    const [user, setUser] = useState({});
+
+    const handleOnClick = (value) => {
+        setUser(value);
+        setVisible(true)
+    }
+
+    const handleCancel = () => {
+        setVisible(false)
+    }
+
+    return (
+        <div id='suggest_list'>
+            <UserCard
+                user={user}
+                isVisible={visible}
+                onCancel={handleCancel}
+            />
+
+            <Row gutter={[16, 16]}>
+                {data.map((ele, index) => {
+                    if (ele.status === 'NOT_FRIEND') {
+                        return (
+                            <Col
+                                span={6}
+                                xl={{ span: 6 }}
+                                lg={{ span: 8 }}
+                                md={{ span: 12 }}
+                                sm={{ span: 12 }}
+                                xs={{ span: 24 }}
+
+                            >
+                                <SuggestCard
+                                    data={ele}
+                                    key={index}
+                                    onClick={handleOnClick}
+                                />
+                            </Col>
+                        )
+                    }
+                })}
+            </Row>
+
+        </div>
+    );
+}
+
+export default SuggestList;
