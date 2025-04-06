fe03919eaa8711968312416918b0572e6b0d08c9
TruongChiBao <chibaotruong1506@gmail.com>
2025-04-06 18:38:45 +0700

    add GroupNews

---
+++ src/screen/Chat/components/ListChannel/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ChannelItem from '../ChannelItem';
+import { useDispatch, useSelector } from 'react-redux';
+import './style.css';
+import { fetchListMessages, getLastViewOfMembers, setCurrentChannel } from '../../slices/chatSlice';
+import { NumberOutlined } from '@ant-design/icons';
+
+ListChannel.propTypes = {
+    data: PropTypes.array,
+};
+
+
+function ListChannel({ data = [] }) {
+    const { currentChannel, currentConversation, conversations } = useSelector(state => state.chat);
+    const dispatch = useDispatch();
+
+    const handleViewGeneralChannel = () => {
+        dispatch(setCurrentChannel(''));
+        dispatch(fetchListMessages({ conversationId: currentConversation, size: 10 }));
+        dispatch(getLastViewOfMembers({ conversationId: currentConversation }));
+
+    }
+
+    return (
+        <div id='list-channel'>
+
+            <div className={`channel-interact-amount ${currentChannel ? '' : 'active'}`} onClick={handleViewGeneralChannel}>
+                <div className="channel-interact-amount-icon">
+                    <NumberOutlined />
+                </div>
+
+                <div className="channel-interact-amount-text">
+                    <span>Kênh chung</span>
+
+                </div>
+                {conversations.find(ele => ele._id === currentConversation).numberUnread > 0 && (
+                    <div className='notify-amount'>
+                        {conversations.find(ele => ele._id === currentConversation).numberUnread}
+                    </div>
+                )}
+
+            </div>
+
+            {data.map((ele, index) => (
+                <ChannelItem
+                    data={ele}
+                    isActive={currentChannel === ele._id ? true : false}
+                />
+            ))}
+
+        </div>
+    );
+}
+
+export default ListChannel;
