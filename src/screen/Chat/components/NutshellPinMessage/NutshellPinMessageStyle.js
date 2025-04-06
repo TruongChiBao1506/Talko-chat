de5793ba3769664d37171b49455314e67430be7b
TruongChiBao <chibaotruong1506@gmail.com>
2025-04-06 18:38:09 +0700

    add DrawerPinMessage

---
+++ src/screen/Chat/components/NutshellPinMessage/NutshellPinMessageStyle.js
@@ -0,0 +1,14 @@
+const NutshellPinMessageStyle = {
+    BUTTON_LIST: {
+        fontSize: '20.8px',
+        borderRadius: '10px',
+        padding: '3.2px 16px',
+        height: 'unset',
+    },
+    MENU_ITEM: {
+        fontSize: '20.8px',
+        fontWeight: '500',
+    },
+};
+
+export default NutshellPinMessageStyle;
