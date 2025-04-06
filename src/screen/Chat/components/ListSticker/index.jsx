a5a753f4f95569255d6bde739766876c79bccc1e
TruongChiBao <chibaotruong1506@gmail.com>
2025-04-06 18:46:29 +0700

    add ListSticker

---
+++ src/screen/Chat/components/ListSticker/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css'
+import StickerItem from '../StickerItem';
+
+ListSticker.propTypes = {
+    data: PropTypes.array,
+    onClose: PropTypes.func,
+    onScroll: PropTypes.func,
+};
+
+function ListSticker({ data = [], onClose = null, onScroll = null }) {
+    return (
+        <div id='sticker-list'>
+            {data.map((ele, index) => (
+                <StickerItem
+                    key={index}
+                    data={ele}
+                    onClose={onClose}
+                    onScroll={onScroll}
+                />
+            ))}
+
+        </div>
+    );
+}
+
+export default ListSticker;
